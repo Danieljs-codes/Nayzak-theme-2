aa8f899da40825faeb4ef5db93d5fc5816945ff8
DanielDartDev <olamideonabiyi@student.oauife.edu.ng>
2023-03-11 06:01:20 +0100

    Initial Commit

---
+++ Travel/tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      screens: {
+        xs: '375px',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        syne: ['syne', 'sans-serif'],
+      },
+      colors: {
+        primary: '#FFE3D2',
+        secondary: '#121212',
+        accent: '#18181E',
+        tertiary: '#212121',
+      },
+    },
+  },
+  plugins: [],
+};
